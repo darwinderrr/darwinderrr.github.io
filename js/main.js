b7e4c2c4ba39978b3e3d58f99a582bc902413dcf
darwinderrr <mdpensotes@gmail.com>
2021-11-26 10:02:39 +0800

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(".nav-toggle").click(function() {
-   if($(this).hasClass("active")) {
-       $(this).removeClass("active");
-       $("div.nav").removeClass("active");
-   } else {
-    $(this).addClass("active");
-    $("div.nav").addClass("active");
-   }
-});
